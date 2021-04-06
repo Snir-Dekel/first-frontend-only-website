887e127dc03e27ecb455ec430f0ab782940a7856
Snir Dekel <66528853+Snir-Dekel@users.noreply.github.com>
2021-04-07 00:15:58 +0300

    slider changes

--- script.js
+++ script.js
@@ -1,51 +1,86 @@
 let header_text = "precision: "
 let num = 0
+let full_answer = "."
+let answer = "."
 fun_for_scale()
 function fun_for_scale() {
+  num = Number(document.getElementById("my_range").value)
   document.getElementById("precision header").textContent =
     header_text + document.getElementById("my_range").value
-  num = Number(document.getElementById("my_range").value)
-  let answer = String(
-    parseFloat(document.getElementById("num1").value) +
-      parseFloat(document.getElementById("num2").value)
-  )
-  document.getElementById("answer").innerText =
-    "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
+
+  final_answer = full_answer.slice(0, full_answer.indexOf(".") + num + 1)
+    if (full_answer.indexOf(".") === -1) {
+    document.getElementById("answer").innerText = "answer=" + full_answer
+      return
+  }
+  console.log(final_answer)
+  console.log(isFinite(final_answer))
+  if (isFinite(final_answer)) {
+    document.getElementById("answer").innerText = "answer=" + final_answer
+  }
 }
 
 function on_click_plus() {
   num = Number(document.getElementById("my_range").value)
-  let answer = String(
+  answer = String(
     parseFloat(document.getElementById("num1").value) +
       parseFloat(document.getElementById("num2").value)
   )
-  document.getElementById("answer").innerText =
-    "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
+    full_answer = answer
+
+  if (answer.indexOf(".") === -1) {
+    document.getElementById("answer").innerText = "answer=" + answer
+    return
+  }
+  if (isFinite(answer)) {
+    document.getElementById("answer").innerText = "answer=" + answer
+  }
 }
+
 function on_click_minus() {
   num = Number(document.getElementById("my_range").value)
-  let answer = String(
+  answer = String(
     parseFloat(document.getElementById("num1").value) -
-      parseFloat(document.getElementById("num2").value)
-  )
-  document.getElementById("answer").innerText =
-    "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
+      parseFloat(document.getElementById("num2").value))
+  full_answer = answer
+
+  if (answer.indexOf(".") === -1) {
+    document.getElementById("answer").innerText = "answer=" + answer
+    return
+  }
+  if (isFinite(answer)) {
+    document.getElementById("answer").innerText = "answer=" + answer
+  }
 }
 function on_click_mul() {
   num = Number(document.getElementById("my_range").value)
-  let answer = String(
+  answer = String(
     parseFloat(document.getElementById("num1").value) *
       parseFloat(document.getElementById("num2").value)
   )
-  document.getElementById("answer").innerText =
-    "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
+    full_answer = answer
+
+  if (answer.indexOf(".") === -1) {
+    document.getElementById("answer").innerText = "answer=" + answer
+    return
+  }
+  if (isFinite(answer)) {
+    document.getElementById("answer").innerText = "answer=" + answer
+  }
 }
 function on_click_div() {
   num = Number(document.getElementById("my_range").value)
-  let answer = String(
+  answer = String(
     parseFloat(document.getElementById("num1").value) /
       parseFloat(document.getElementById("num2").value)
   )
-  document.getElementById("answer").innerText =
-    "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
+    full_answer = answer
+
+  if (answer.indexOf(".") === -1) {
+    document.getElementById("answer").innerText = "answer=" + answer
+    return
+  }
+  if (isFinite(answer)) {
+    document.getElementById("answer").innerText = "answer=" + answer
+  }
 }
