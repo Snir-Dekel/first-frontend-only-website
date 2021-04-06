138d0c22444ac7ffb55bc03f1a087b7a97d61485
Snir Dekel <66528853+Snir-Dekel@users.noreply.github.com>
2021-04-06 22:42:27 +0300

    second commit

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ fun_for_scale()
 function fun_for_scale() {
   document.getElementById("precision header").textContent =
     header_text + document.getElementById("my_range").value
+  num = Number(document.getElementById("my_range").value)
+  let answer = String(
+    parseFloat(document.getElementById("num1").value) +
+      parseFloat(document.getElementById("num2").value)
+  )
+  document.getElementById("answer").innerText =
+    "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
 }
 
 function on_click_plus() {
@@ -42,4 +49,3 @@ function on_click_div() {
   document.getElementById("answer").innerText =
     "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
 }
-
