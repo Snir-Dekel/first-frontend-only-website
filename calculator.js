60d0715c0d92388a2750dc5b8919db92d1521df7
Snir Dekel <66528853+Snir-Dekel@users.noreply.github.com>
2021-05-13 21:11:36 +0300

    added input sliders that chages the 3d direction

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ let header_text = "precision: "
 let num = 0
 let full_answer = "."
 let answer = "."
+
 fun_for_scale()
 function fun_for_scale() {
   num = Number(document.getElementById("my_range").value)
@@ -9,7 +10,7 @@ function fun_for_scale() {
     header_text + document.getElementById("my_range").value
   let final_answer = full_answer.slice(0, full_answer.indexOf(".") + num + 1)
   console.log(final_answer)
-  if (!isFinite(final_answer) && final_answer!=="-") {
+  if (!isFinite(final_answer) && final_answer !== "-") {
     document.getElementById("answer").innerText = "please fill the input"
     return
   }
@@ -56,4 +57,18 @@ function main_function(operand) {
   }
   document.getElementById("answer").innerText =
     "answer=" + answer.slice(0, answer.indexOf(".") + num + 1)
+}
+function on_range_change_3d(direction) {
+  if (direction==="upper_left"){
+    document.querySelector("#sign_element").style.transform = `rotate3d(1, 1, 0, ${document.querySelector("#upper_left").value * 10}deg)`
+  }
+    if (direction==="upper_right"){
+    document.querySelector("#sign_element").style.transform = `rotate3d(-1, 1, 0, ${document.querySelector("#upper_right").value * 10}deg)`
+  }
+    if (direction==="bottom_right"){
+    document.querySelector("#sign_element").style.transform = `rotate3d(-1, -1, 0, ${document.querySelector("#bottom_right").value * 10}deg)`
+  }
+        if (direction==="bottom_left"){
+    document.querySelector("#sign_element").style.transform = `rotate3d(1, -1, 0, ${document.querySelector("#bottom_left").value * 10}deg)`
+  }
 }
